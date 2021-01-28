d6d5eeaf0b734bc9abb58e6cd566e284f37203f5
muhamad akbar <77180979+Akbarsans@users.noreply.github.com>
2021-01-28 07:11:05 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,59 @@
+const help = (prefix) => { 
+	return `
+
+*▌║========[MENU]========*
+➬ 
+➬  *${prefix}ownermenu*
+➬  *${prefix}adminmenu*
+➬  *${prefix}funmenu*
+➬  *${prefix}mediamenu*
+➬  *${prefix}kerangmenu*
+➬  *${prefix}makermenu*
+➬  *${prefix}othermenu*
+➬  *${prefix}animemenu*
+➬  *${prefix}nsfwmenu*
+➬  *${prefix}vipmenu*
+➬ 
+*▌║=======================*
+➬ 
+➬  *${prefix}listmenu*
+➬ 
+*▌║⊱⊲ ⃟ ⃟ ⃟ ⛨*
+*➬ ${prefix}bugreport*
+*➬ *  *${prefix}info*
+*➬ ${prefix}owner*
+➬ 
+*▌║⊱⊲ ⃟ ⃟ ⃟ ⛨*
+➬ 
+*➬ ${prefix}request*
+*➬ ${prefix}setprefix*
+*➬ ${prefix}listblock*
+➬ 
+*▌║⊱⊲ ⃟ ⃟ ⃟ ⛨*
+➬ 
+*➬ ${prefix}iklan*
+*➬ ${prefix}runtume*
+*➬ ${prefix}rules*
+*➬ ${prefix}tnc*
+*➬ ${prefix}cekvip*
+➬ 
+*▌║⊱⊲ ⃟ ⃟ ⃟ ⛨*
+➬ 
+*➬ ${prefix}daftarvip*
+*➬ ${prefix}addvip*
+*➬ ${prefix}dellvip*
+*➬ ${prefix}snk*
+*➬ ${prefix}listpremium*
+*➬ ${prefix}donate*
+*➬ ${prefix}fitnah*
+*➬ ${prefix}totaluser*
+*➬ ${prefix}level*
+*➬ ${prefix}leveling*
+*➬ ${prefix}addbacot*
+*➬ ${prefix}bacotlist*
+*➬ ${prefix}resetbacot*
+➬ 
+*▌║======[Mirai chan]======*
+`
+}
+exports.help = help
